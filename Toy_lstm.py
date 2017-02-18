805270e17e18b468098ce2a403990f55f6a477c4
Youwei Zhu <1807279653@qq.com>
2017-02-18 16:17:54 +0800

    a

--- Toy_lstm.py
+++ Toy_lstm.py
@@ -145,13 +145,13 @@ class Train_by_LSTM(object):
 		
 		
 		time.sleep(5)
-		# my_early_stop=keras.callbacks.EarlyStopping(monitor='train_loss', min_delta=0, patience=20, verbose=0, mode='auto')
-		# self.model.fit(x=self.X_train,y=self.Y_train,batch_size=self.batch_size,nb_epoch=self.Epochs,callbacks=[my_early_stop])
-		self.model.fit(x=self.X_train,y=self.Y_train,batch_size=self.batch_size,nb_epoch=self.Epochs)
+		my_early_stop=keras.callbacks.EarlyStopping(monitor='train_loss', min_delta=0, patience=20, verbose=0, mode='auto')
+		self.model.fit(x=self.X_train,y=self.Y_train,batch_size=self.batch_size,nb_epoch=self.Epochs,callbacks=[my_early_stop],validation_split=0.25)
+		# self.model.fit(x=self.X_train,y=self.Y_train,batch_size=self.batch_size,nb_epoch=self.Epochs)
 
 	def perform_outsample_test(self):
 		print ("ideal_loss_func,var_Y",self.ideal_loss_func,self.var_Y)
-		test_result=self.model.evaluate(self.X_test,self.Y_test, batch_size=self.batch_size,validation_data=(self.X_test,self.Y_test))
+		test_result=self.model.evaluate(self.X_test,self.Y_test, batch_size=self.batch_size)
 		print ("out-sample test_result",test_result)
 	def launch_test(self):
 		pass
