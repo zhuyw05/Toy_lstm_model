6d635bc33959145437aa926f197a121ec14cb670
Youwei Zhu <1807279653@qq.com>
2017-02-18 14:13:14 +0800

    a

--- Toy_lstm.py
+++ Toy_lstm.py
@@ -139,11 +139,11 @@ class Train_by_LSTM(object):
 		
 		
 		time.sleep(5)
-		self.model.fit(x=self.X_train,y=self.Y_train,batch_size=self.batch_size,nb_epoch=self.Epochs)
+		self.model.fit(x=self.X_train,y=self.Y_train,batch_size=self.batch_size,nb_epoch=self.Epochs,callbacks=self.perform_outsample_test)
 
 	def perform_outsample_test(self):
 		test_result=self.model.evaluate(self.X_test,self.Y_test, batch_size=self.batch_size)
-		print ("test_result",test_result)
+		print ("out-sample test_result",test_result)
 	def launch_test(self):
 		pass
 		
